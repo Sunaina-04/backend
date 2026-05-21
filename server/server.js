d96b0a28daf9999fbb0acef7377d7354fd79b342
Sunaina <sunaina0798.becse24@chitkara.edu.in>
2026-05-22 00:19:31 +0530

    Updated fix files

--- server/server.js
+++ server/server.js
@@ -54,7 +54,13 @@ app.set('io', io);
 
 // 5. Global Middleware
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.use((req, res, next) => {
+    if (req.method === 'OPTIONS') {
+        return cors(corsOptions)(req, res, () => res.sendStatus(204));
+    }
+
+    return next();
+});
 
 app.use(express.json()); 
 app.use(cookieParser()); 
